86334fead8eb14eeb510222ef7131b1f415807f3
Matt Blair <blairmat@sheridanc.com>
2022-04-15 16:20:59 -0700

    player and hand

--- Hand.java
+++ Hand.java
@@ -1,3 +1,38 @@
+import java.util.ArrayList;
+
 public class Hand {
+
+    private ArrayList<Card> cards;
+
+    public Hand() {
+        this.cards = new ArrayList<Card>();
+    }
+
+    public void addCard(Card card) {
+        this.cards.add(card);
+    }
+
+    public void removeCard(Card card) {
+        this.cards.remove(card);
+    }
     
+    public int getSize() {
+        return this.cards.size();
+    }
+
+    public ArrayList<Card> getCards() {
+        return this.cards;
+    }
+
+    public void setCards(ArrayList<Card> cards) {
+        this.cards = cards;
+    }
+
+    public int getCardsInHand() {
+        int value = 0;
+        for (Card card : this.cards) {
+            value += card.getCardsInHand();
+        }
+        return value;
+    }
 }
--- Player.java
+++ Player.java
@@ -1,3 +1,45 @@
 public class Player {
     
+    private String name;
+    private Hand hand;
+    private int id;
+
+    public Player(String name) {
+        this.name = name;
+        this.hand = new Hand();
+        this.id = generateId();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Hand getHand() {
+        return hand;
+    }
+
+    public void setHand(Hand hand) {
+        this.hand = hand;
+    }
+
+    public void addCard(Card card) {
+        this.hand.addCard(card);
+    }
+
+    public void removeCard(Card card) {
+        this.hand.removeCard(card);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int generateId() {
+        this.id = (int) (Math.random() * 100);
+        return this.id;
+    }
 }
