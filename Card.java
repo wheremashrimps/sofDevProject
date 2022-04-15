5ea358cfe0bc251d4a914fa1eea3913af74473eb
ananpune <97976163+ananpune@users.noreply.github.com>
2022-04-15 19:20:10 -0400

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,3 +1,48 @@
 public class Card {
     
+    public enum Suit{
+            HEARTS,
+            SPADES,
+            DIAMONDS,
+            CLUBS, 
+    }
+    
+    public enum Rank{
+            ACE,
+            TWO,
+            THREE,
+            FOUR,
+            FIVE,
+            SIX,
+            SEVEN,
+            EIGHT,
+            NINE,
+            TEN,
+            JACK,
+            QUEEN,
+            KING
+    }
+     public Card (Suit suit, Rank rank){
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public Suit getSuit() {
+        return this.suit;
+    }
+
+    public Rank getRank(){
+        return this.rank;
+    }
+    public void setSuit(Suit suit) {
+        this.suit=suit;;
+    }
+
+    public void setRank(Rank rank){
+        this.rank=rank;
+    }
+    @Override
+    public String toString(){
+        return this.rank + " of " + this.suit;
+    }
 }
